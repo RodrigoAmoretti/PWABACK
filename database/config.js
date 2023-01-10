40b81173696ad0314d537c53184b1a12dc441762
Rodrigo <rodrigo695@hotmail.com>
2023-01-10 17:08:18 -0300

    2

--- database/config.js
+++ database/config.js
@@ -1,10 +1,17 @@
+import {
+    HOST_DB,
+    PASSWORD_DB,
+    USER_DB,
+    PORT_DB,
+    DATABASE_DB
+}from '../configs.js'
 import { createPool } from 'mysql2/promise'
 
 export const pool = createPool({
-    host:HOST_DB,
-    port:PORT_DB,
-    user:USER_DB,
-    password:PASSWORD_DB,
-    database:DATABASE_DB 
+    host: HOST_DB,
+    port: PORT_DB,
+    user: USER_DB,
+    password: PASSWORD_DB,
+    database: DATABASE_DB
 })
 
